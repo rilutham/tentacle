6a8d19bbda34a044ad1634b042dada45dd7f5324
Riky Lutfi Hamzah <rlh_258@yahoo.co.id>
2015-09-12 14:53:01 +0700

    update add hour form

--- tentacle/forms.py
+++ tentacle/forms.py
@@ -1,7 +1,8 @@
 from django import forms
 
 class IncreaseHourForm(forms.Form):
-    num = forms.CharField(max_length=4, label=False)
+    CHOICES = [('pos', 'Add'), ('neg', 'Sub')]
+    choice_field = forms.ChoiceField(widget=forms.RadioSelect(), choices=CHOICES, initial='pos')
+    num = forms.CharField(max_length=4)
+    #note = forms.CharField(max_length=140)
 
-class DecreaseHourForm(forms.Form):
-	num = forms.CharField(max_length=4, label=False)
--- tentacle/views.py
+++ tentacle/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect
 from .models import Activity
-from .forms import IncreaseHourForm, DecreaseHourForm
+from .forms import IncreaseHourForm
 
 def index(request):
     activities = Activity.objects.order_by('-total_hour')
@@ -11,34 +11,29 @@ def index(request):
 
 def detail(request, activity_id):
     activity = get_object_or_404(Activity, pk=activity_id)
-    
+
     # Increase and decrease total hour form.
     if request.method == 'POST':
         if 'inc_hour_btn' in request.POST:
             increase_hour_form = IncreaseHourForm(request.POST)
             if increase_hour_form.is_valid():
-                activity.latest_hour = int(increase_hour_form.cleaned_data['num'])
+                choice = increase_hour_form.cleaned_data['choice_field']
+                if choice == 'pos':
+                    num = int(increase_hour_form.cleaned_data['num'])
+                elif choice == 'neg':
+                    num = -int(increase_hour_form.cleaned_data['num'])
+                activity.latest_hour = num
                 activity.update_total_hour()
                 activity.get_last_update()
                 activity.save()
                 return HttpResponseRedirect('/' + activity_id)
-        elif 'dec_hour_btn' in  request.POST:
-            decrease_hour_form = DecreaseHourForm(request.POST)
-            if decrease_hour_form.is_valid():
-                activity.latest_hour = -int(decrease_hour_form.cleaned_data['num'])
-                activity.update_total_hour()
-                activity.get_last_update()
-                activity.save()
-                return HttpResponseRedirect('/' + activity_id)  
     else:
         increase_hour_form = IncreaseHourForm()
-        decrease_hour_form = DecreaseHourForm()
 
     activities = Activity.objects.order_by('-total_hour')
     activity_list = activities.filter(parent_activity_id = 0)
     top_level_activity = activity_list.exclude(id=0)
 
     return render(request, 'tentacle/detail.html', {'activity': activity, \
-    	'increase_hour_form': increase_hour_form, 'decrease_hour_form': decrease_hour_form, \
-    	'top_level_activity': top_level_activity})
+            'increase_hour_form': increase_hour_form, 'top_level_activity': top_level_activity})
 
