7b7bb7daf9c2f7663532266763d529937fd47503
Riky Lutfi Hamzah <rlh_258@yahoo.co.id>
2015-09-13 09:11:18 +0700

    update namespace

--- tentacle_project/urls.py
+++ tentacle_project/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^', include('tentacle.urls', namespace="tentacle_ns")),
+    url(r'^', include('tentacle.urls', namespace='tentacle_ns')),
 ]
 
