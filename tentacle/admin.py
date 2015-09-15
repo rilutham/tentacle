31639b14bb0b1ab5c538fbf9d2900cddaa3d10f5
Riky Lutfi Hamzah <rlh_258@yahoo.co.id>
2015-09-15 12:10:20 +0700

    add note model to admin

--- tentacle/admin.py
+++ tentacle/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from .models import Activity
+from .models import Activity, Note
 
 class ActivityAdmin(admin.ModelAdmin):
     list_display = ('activity_name','description','total_hour','latest_hour','created_at','updated_at')
 
+class NoteAdmin(admin.ModelAdmin):
+	list_display = ('activity_id', 'note','status','created_at')
+
 admin.site.register(Activity, ActivityAdmin)
+admin.site.register(Note, NoteAdmin)
